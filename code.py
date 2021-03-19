cdba88d137094dde4e350d093902336b1aa6387e
Paul30hub <paulburgat5@gmail.com>
2021-03-19 02:15:19 +0100

    code_du_challenge

---
+++ code.py
@@ -0,0 +1,9 @@
+#%%
+import pandas as pd
+pd.read_csv('La myriade de Totems de Montpellier - SaisiesFormulaire.csv', sep=",")
+
+# %%
+#tableau de données 2021
+pd.read_csv('La myriade de Totems de Montpellier - SaisiesFormulaire.csv',skiprows=1153, sep=',')
+
+# %%
