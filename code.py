4d51ff492332a103a2413590c36dbb8254287337
Paul30hub <paulburgat5@gmail.com>
2021-03-31 03:06:48 +0200

    prediction nbr velos

--- code.py
+++ code.py
@@ -1,9 +1,16 @@
 #%%
-import pandas as pd
-pd.read_csv('La myriade de Totems de Montpellier - SaisiesFormulaire.csv', sep=",")
-
+import pandas as pd 
+l=[i for i in range(1,1154)]
+data = pd.read_csv("La myriade de Totems de Montpellier.csv", sep=",", skiprows=l)
+data # On prend en compte les données seulement de 2021
+#%% 
+data_velos = pd.read_excel("Classeur1.xlsx")
+#On utilise un tablau qui contient les observations aux alentours de 9h les jours de la semaine
+print(data_velos)
+#%%
+import datetime as dt 
+d1 = dt.time(12, 56, 42)
 # %%
-#tableau de données 2021
-pd.read_csv('La myriade de Totems de Montpellier - SaisiesFormulaire.csv',skiprows=1153, sep=',')
-
+import numpy as np 
+np.mean(data_velos['Nbr vélos'])
 # %%
