2be69ba2a6aea8f000997c18cf21778e407d36a2
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-03-31 03:19:18 +0200

    Update code.py

--- code.py
+++ code.py
@@ -5,12 +5,9 @@ data = pd.read_csv("La myriade de Totems de Montpellier.csv", sep=",", skiprows=
 data # On prend en compte les données seulement de 2021
 #%% 
 data_velos = pd.read_excel("Classeur1.xlsx")
-#On utilise un tablau qui contient les observations aux alentours de 9h les jours de la semaine
+#On utilise un tablau qui contient le nombre de vélos pour une moyenne de 09:01:00 les jours de la semaine en 2021
 print(data_velos)
 #%%
-import datetime as dt 
-d1 = dt.time(12, 56, 42)
-# %%
 import numpy as np 
-np.mean(data_velos['Nbr vélos'])
-# %%
+np.mean(data_velos['Nbr vélos']) #On fait une moyenne du nombre de vélos obtenus ce qui nous donne le résultat de 252
+
