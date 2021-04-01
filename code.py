d3f4678d44fb4a3639228c646458bd607a5c816e
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-04-01 23:39:43 +0200

    Update code.py

--- code.py
+++ code.py
@@ -5,12 +5,11 @@ data = pd.read_csv("La myriade de Totems de Montpellier.csv", sep=",", skiprows=
 data # On prend en compte les données seulement de 2021
 #%% 
 data_velos = pd.read_excel("Classeur1.xlsx")
-#On utilise un tablau qui contient les observations aux alentours de 9h les jours de la semaine
+#On utilise un tableau qui contient les observations aux alentours de 9h les jours de la semaine
 print(data_velos)
-#%%
-import datetime as dt 
-d1 = dt.time(12, 56, 42)
 # %%
 import numpy as np 
-np.mean(data_velos['Nbr vélos'])
+np.mean(data_velos['Nbr vélos']) # On prend la moyenne du nombre de vélos par jour entre 00h00 et 09h00
+#Cette moyenne est 313,36, je fais la prédiction qu'il y aura donc 313 vélos le 02 avril 2021
+#au compteur d'Albert 1er entre 00h00 et 09h00
 # %%
